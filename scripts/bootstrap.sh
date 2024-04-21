522b82ea112f3c807cbbcdd1fb8e6568b557c693
chainyo <t.chaigneau.tc@gmail.com>
2024-04-21 18:13:46 +0200

    fix path to download script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,9 +3,11 @@ set -euo pipefail
 
 # This wraps everything to avoid truncated script issues.
 __wrap__() {
+    # Determine the directory this script resides in
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
     # Download or use cached binary
-    BINARY_PATH=$(./download.sh)
+    BINARY_PATH=$("$SCRIPT_DIR/download.sh")
 
     # Execute the binary with all passed arguments
     if [ -x "$BINARY_PATH" ]; then
@@ -14,7 +16,6 @@ __wrap__() {
         echo "Failed to execute the binary."
         exit 1
     fi
-
 }
 
 __wrap__
