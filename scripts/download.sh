925804d328f281750fc7e3584425eb48473e2add
chainyo <t.chaigneau.tc@gmail.com>
2024-04-21 18:37:35 +0200

    fix download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -6,27 +6,43 @@ REPO="chainyo/py-init-cleaner"
 PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
-case $PLATFORM in
-    Darwin) PLATFORM="macos" ;;
-    Linux) PLATFORM="linux" ;;
-esac
+if [[ $PLATFORM == "darwin" ]]; then
+  PLATFORM="macos"
+fi
 
-case $ARCH in
-    armv8*|arm64*|aarch64*) ARCH="aarch64" ;;
-    i686*) ARCH="x86" ;;
-    *) ARCH="x86_64" ;;
-esac
+if [[ $ARCH == armv8* ]] || [[ $ARCH == arm64* ]] || [[ $ARCH == aarch64* ]]; then
+  ARCH="aarch64"
+elif [[ $ARCH == i686* ]]; then
+  ARCH="x86"
+fi
 
 BINARY="py-init-cleaner-${ARCH}-${PLATFORM}"
 BINARY_PATH="$HOME/.cache/pre-commit/$BINARY"
 
+# Oddly enough GitHub has different URLs for latest vs specific version
+if [[ $VERSION == "latest" ]]; then
+  DOWNLOAD_URL=https://github.com/${REPO}/releases/latest/download/${BINARY}.gz
+else
+  DOWNLOAD_URL=https://github.com/${REPO}/releases/download/${VERSION}/${BINARY}.gz
+fi
+
 # Check if binary is already downloaded
 if [[ ! -f "$BINARY_PATH" ]]; then
     echo "Downloading $BINARY..."
-    DOWNLOAD_URL="https://github.com/${REPO}/releases/download/${VERSION}/${BINARY}.gz"
+    echo "Downloading from: $DOWNLOAD_URL"
     curl -SL --progress-bar "$DOWNLOAD_URL" -o "${BINARY_PATH}.gz"
-    gunzip "${BINARY_PATH}.gz"
-    chmod +x "$BINARY_PATH"
+
+    # Ensure the file is in gzip format
+    file "${BINARY_PATH}.gz" # This will print the file type
+
+    # Proceed only if the file is a gzip file
+    if file "${BINARY_PATH}.gz" | grep -q 'gzip compressed data'; then
+        gunzip "${BINARY_PATH}.gz"
+        chmod +x "$BINARY_PATH"
+    else
+        echo "Downloaded file is not in gzip format"
+        exit 1
+    fi
 else
     echo "Using cached binary."
 fi
